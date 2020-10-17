5c57042c5be7bfd8c1c120e5980942ce0b227753
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-17 06:23:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_list={}
+
+  data.each do |outer_key,values| #Access lvl1
+    values.each do |inner_key,p_names| #Access lvl2
+      p_names.each do |name| #Access lvl3
+        if organized_list[name] ||={} #using||= to check for nil assign empty hash
+          if organized_list[name][outer_key] ||= [] #using||= to check for nil assign empty array
+            organized_list[name][outer_key]<< (inner_key.to_s) #Convert symbol to strings
+      end
+     end
+    end
+   end
+ end
+ organized_list
 end
