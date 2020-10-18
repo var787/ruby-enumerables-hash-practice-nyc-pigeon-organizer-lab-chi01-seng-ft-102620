a615026ef166258e172e7636dc85dcecdc17ec1f
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-18 23:41:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,11 +4,14 @@ def nyc_pigeon_organizer(data)
   organized_list={}
 
   data.each do |outer_key,values| #Access lvl1
+    binding.pry
     values.each do |inner_key,p_names| #Access lvl2
+      binding.pry
       p_names.each do |name| #Access lvl3
         if organized_list[name] ||={} #using||= to check for nil assign empty hash
           if organized_list[name][outer_key] ||= [] #using||= to check for nil assign empty array
             organized_list[name][outer_key]<< (inner_key.to_s) #Convert symbol to strings
+            binding.pry
       end
      end
     end
